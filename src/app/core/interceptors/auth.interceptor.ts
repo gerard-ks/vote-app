b09301db46d9c41d295a3a4153e0169cadf29d9d
Siaka Kouakou Gérard <siaka.kouakou.gerard@gmail.com>
2026-07-03 01:38:57 +0000

    chore(core): clean up project structure and remove unused folders

--- src/app/core/interceptors/auth.interceptor.ts
+++
@@ -1,8 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const secureReq = req.clone({
-    withCredentials: true, // Obligation pour l'authentification HttpOnly sécurisée inter-domaines
-  });
-  return next(secureReq);
-};
--- src/app/core/interceptors/zod-context.ts
+++
@@ -1,4 +0,0 @@
-import { HttpContextToken } from '@angular/common/http';
-import { ZodType } from 'zod';
-
-export const Z_SCHEMA = new HttpContextToken<ZodType<unknown, any, any> | null>(() => null);
